b7670465b9b07b02848d0d4c0ca6e67a7885d825
HitendraKawale <hituhitesh303@gmail.com>
2026-03-09 06:35:54 +0530

    Added pdf upload and text extraction service

--- ai-document-intelligence/app/api/routes.py
+++ ai-document-intelligence/app/api/routes.py
@@ -1,9 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
+import os 
+
+from app.services.pdf_parser import extract_text_from_pdf
 
 router = APIRouter()
 
-@router.get("/")
+UPLOAD_DIR = "data/uploads"
 
+@router.post("/upload-pdf/")
 def health_check():
-    return {"status": "running"}
+    return {"message": "API is working!"}
 
+@router.post("/upload/")
+async def upload_pdf(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    #Save the uploaded file to the server
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    # Extract text from the uploaded PDF
+    extracted_text = extract_text_from_pdf(file_path)
+
+    return {
+        "filename": file.filename,
+        "extracted_text": extracted_text,
+        "text_length": len(extracted_text)
+    }
---
+++ ai-document-intelligence/app/services/pdf_parser.py
@@ -0,0 +1,14 @@
+import fitz
+
+def extract_text_from_pdf(file_path: str) -> str:
+    # Open the PDF file
+    document = fitz.open(file_path)
+    # Initialize an empty string to hold the extracted text
+    extracted_text = ""
+    
+    for page in document:
+        extracted_text += page.get_text()
+        
+    document.close()
+    return extracted_text
+    
