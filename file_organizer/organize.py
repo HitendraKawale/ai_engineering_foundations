1ee9196a014f4ade768f182d02084e3794d456b4
HitendraKawale <hituhitesh303@gmail.com>
2026-06-05 00:41:04 +0100

    file organiser

--- file_organizer/organize.py
+++ file_organizer/organize.py
@@ -1,9 +1,21 @@
 import os
+import shutil
 
 path = "test_files"
 
 list_of_files = os.listdir(path)
 
+os.makedirs("results", exist_ok=True)
+
+print(list_of_files)
+
 for i in list_of_files:
     name, ext = os.path.splitext(i)
-    print(name, ext)
+
+    ext_clean = ext[1:]
+
+    os.makedirs(os.path.join("results", ext_clean), exist_ok=True)
+
+    source = os.path.join(path, i)
+    destination = os.path.join("results", ext_clean, i)
+    shutil.move(source, destination)
